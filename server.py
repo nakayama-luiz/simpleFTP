daa628f0d893ba62ce8509039d341ddeea7b00d1
provavelmente <110038923+nakayama-luiz@users.noreply.github.com>
2023-12-26 12:42:01 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+authorizer = DummyAuthorizer()
+authorizer.add_user("username", "password", "C:\\Users\\T-GAMER\\Documents\\nano", perm="elradfmw")
+
+handler = FTPHandler
+handler.authorizer = authorizer
+
+server = FTPServer(("0.0.0.0", 21), handler)
+
+server.serve_forever()
